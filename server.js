3667eca5a4381954adc57e84e892666482937fb8
이준희 <dev.junhui@gmail.com>
2024-02-25 01:37:45 +0900

    FEAT : 무한 스크롤 구현

--- server.js
+++ server.js
@@ -28,22 +28,22 @@ async function connectToMongoDB() {
 
 connectToMongoDB();
 
-const PAGE_SIZE = 3;
-
 app.get('/article', async (req, res) => {
-  const pageNumber = req.query.pageNumber ? parseInt(req.query.pageNumber) : 1;
-
   try {
-    const articles = await articleCollection
-      .find({})
-      .sort({ _id: -1 }) // _id를 기준으로 역순으로 정렬
-      .skip((pageNumber - 1) * PAGE_SIZE)
-      .limit(PAGE_SIZE)
-      .toArray();
-
+    const articles = await articleCollection.find({}).toArray();
     res.json({ result: 'success', article: articles });
   } catch (error) {
     console.error('Error fetching articles:', error);
+    res.status(500).json({ result: 'error', message: 'Internal server error' });
+  }
+});
+
+app.get('/rank', async (req, res) => {
+  try {
+    const ranks = await rankCollection.find({}).toArray();
+    res.json({ result: 'success', rank: ranks });
+  } catch (error) {
+    console.error('Error fetching ranks:', error);
     res.status(500).json({ result: 'error', message: 'Internal server error' });
   }
 });
